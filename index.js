874c66cbfd4476d4fb67398a065be32c314ac046
WarningShoot <damian19956@gmail.com>
2023-10-06 16:33:01 +0200

    final

--- index.js
+++ index.js
@@ -1,7 +1,66 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+
+const uri =
+  "mongodb+srv://admin:haslo1@cluster0.wtsvqmh.mongodb.net/?retryWrites=true&w=majority";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+app.get("/checkauthorization/:username", async (req, res) => {
+  await client.connect();
+
+  const username = req.params.username;
+
+  const collection = client.db("users").collection("users");
+
+  const query = { username };
+
+  const user = await collection.findOne(query);
+
+  await client.close();
+
+  return res.json(user);
+});
+
+app.get("/adduser/:username", async (req, res) => {
+  await client.connect();
+
+  const username = req.params.username;
+
+  const collection = client.db("users").collection("users");
+
+  const newUser = {
+    username,
+  };
+
+  await collection.insertOne(newUser);
+
+  await client.close();
+
+  return res.json("User created");
+});
+
+app.get("/removeuser/:username", async (req, res) => {
+  await client.connect();
+
+  const username = req.params.username;
+
+  const collection = client.db("users").collection("users");
+
+  const query = { username };
+
+  await collection.deleteOne(query);
+
+  await client.close();
+
+  return res.json("User deleted");
+});
+
+app.listen(process.env.PORT || 3000);
